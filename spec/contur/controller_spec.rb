9f83bf0b9d8ab8971c105c039bde4ca06eddaedc
Gabor Takacs <gabor.takacs@cheppers.com>
2016-11-25 13:56:31 +0100

    Fix rubocop error

--- spec/contur/controller_spec.rb
+++ spec/contur/controller_spec.rb
@@ -3,9 +3,9 @@ require 'yaml'
 require 'contur/controller'
 
 describe Contur::Controller do
-it '#default_yaml' do
-  expect(Contur::Controller.default_yaml).to_not be_empty
-  expect(Contur::Controller.default_yaml).to be_a String
-  expect{ YAML.parse(Contur::Controller.default_yaml) }.to_not raise_error Psych::SyntaxError
-end
+  it '#default_yaml' do
+    expect(Contur::Controller.default_yaml).to_not be_empty
+    expect(Contur::Controller.default_yaml).to be_a String
+    expect { YAML.parse(Contur::Controller.default_yaml) }.to_not raise_error Psych::SyntaxError
+  end
 end
